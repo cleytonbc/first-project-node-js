2a1d80404060e314e915588f9eb2bc9fd2a92adc
Cleyton <cleytonbc@hotmail.com>
2020-10-17 18:21:11 -0300

    nodenon e incluindo CRUD

--- index.js
+++ index.js
@@ -2,12 +2,53 @@ const express = require('express');
 
 const server = express();
 
-const users = ['Cleyton', 'Maria', 'Luiz'];
+// Express reconhecer o json no post
+server.use(express.json());
+
+// CRUD - Create - Read - Update - Delete
+
+const users = ['Cleyton', 'Maria', 'Luiz', 'Fatinha'];
+
+// Read
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
 
 server.get('/users/:index', (req, res) => { 
   const { index } = req.params;
 
   return res.json({message : `Exibindo o indice ${index} ` + users[index]});
+});
+
+// Create -request body
+server.post('/users', (req,res) =>{
+  const { name } =req.body;
+
+  users.push(name);
+
+  return res.json(users);
+
+});
+
+// Update
+
+server.put('/users/:index',(req,res)=>{
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+})
+
+// Delete
+
+server.delete("/users/:index", (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send ();
 })
 
 server.listen(3000);
