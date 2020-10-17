11916a4f247856677af0f8acfa77af8afb0f88c2
Cleyton <cleytonbc@hotmail.com>
2020-10-17 13:36:32 -0300

    Acrescentando  API Rest utilizando parametro

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const server = express();
 
-server.get('/teste', (req, res) => { 
-  return res.json({message : 'Hello Word'});
+const users = ['Cleyton', 'Maria', 'Luiz'];
+
+server.get('/users/:index', (req, res) => { 
+  const { index } = req.params;
+
+  return res.json({message : `Exibindo o indice ${index} ` + users[index]});
 })
 
 server.listen(3000);
