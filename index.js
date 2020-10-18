9e96600c6a38edbd15dc04fef53d368586a2079e
Cleyton <cleytonbc@hotmail.com>
2020-10-18 10:15:48 -0300

    Middleware Glocal e Local

--- index.js
+++ index.js
@@ -5,23 +5,57 @@ const server = express();
 // Express reconhecer o json no post
 server.use(express.json());
 
+// Middleware Global
+
+server.use((req,  res, next)  =>  {
+  console.time('request');
+  console.log(`Metodo: ${req.method}, url ${req.url} `);
+
+ next();
+
+ console.timeEnd('request');
+});
+
+//Middleware Local
+function checkUserNameExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json ( { erro: 'User name is requerid'})
+  }
+  return next();
+}
+
+function checkUserInArray (req, res, next ) {
+  const user = users[req.params.index]
+
+  if  (!user) {
+    return res.status(400).json({error: "user does not exists"});
+  }
+
+  req.user=user;  
+
+  return next();
+}
+
 // CRUD - Create - Read - Update - Delete
 
 const users = ['Cleyton', 'Maria', 'Luiz', 'Fatinha'];
 
 // Read
+// Buscar  todos usuários
 server.get('/users', (req, res) => {
   return res.json(users);
 });
 
-server.get('/users/:index', (req, res) => { 
-  const { index } = req.params;
+// Buscar um usuário
+server.get('/users/:index', checkUserInArray, (req, res) => { 
+ // const { index } = req.params;
 
-  return res.json({message : `Exibindo o indice ${index} ` + users[index]});
+  return res.json({message : `Exibindo o usuário ${req.user} `});
 });
 
 // Create -request body
-server.post('/users', (req,res) =>{
+// Criando usuário
+server.post('/users', checkUserNameExists, (req, res) =>{
   const { name } =req.body;
 
   users.push(name);
@@ -31,8 +65,8 @@ server.post('/users', (req,res) =>{
 });
 
 // Update
-
-server.put('/users/:index',(req,res)=>{
+// Editar usuário
+server.put('/users/:index', checkUserInArray, checkUserNameExists, (req, res) =>{
   const { index } = req.params;
   const { name } = req.body;
 
@@ -42,7 +76,7 @@ server.put('/users/:index',(req,res)=>{
 })
 
 // Delete
-
+// Excluir usuário
 server.delete("/users/:index", (req, res) => {
   const { index } = req.params;
 
